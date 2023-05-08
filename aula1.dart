dd2514a49fb7553956895fc4b2583ff813c309ac
unknown <ETEC@ETEC.FURLAN>
2023-05-08 12:37:39 -0300

    Aula basica de dart

---
+++ aula1.dart
@@ -0,0 +1,19 @@
+void main() {
+//Tipos básicos de dados
+
+  int idade = 17;
+  String nome = "Samuel Santos";
+
+//Declaração de listas
+  List<int> idadesAlunos = [14, 15, 16, 17, 18];
+  List<String> nomeAlunos = ["Maria", "Yasmin", "Vitor", "Rogers", "Isabelly"];
+
+// Loop (for)
+
+  for (int i = 0; i < 6; i++) {
+    print(i);
+  }
+}
+
+
+//proxima aula, percorrer uma lista
---
+++ basic1.dart
@@ -0,0 +1,24 @@
+void main() {
+  List<String> names = [
+    "Samuel",
+    "Maria",
+    "Joaquim",
+    "Felipe",
+    "Toni",
+    "Erick",
+    "Ana",
+    "João",
+  ];
+
+ 
+
+  for (int index = 0; index <= names.length; index++) {
+    print('hello ${names[index]}');
+  }
+}
+
+
+
+  
+
+ 
